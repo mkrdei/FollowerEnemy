480926ad14a81232b85082c0fe34e5919925fe12
Muhammet İkbal Karadayı <muhammetkaradayi@hotmail.com>
2022-09-07 22:48:11 +0300

    Initial commit

---
+++ Assets/Enemy.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    CharacterController characterController;
+    Rigidbody rb;
+    private GameObject player;
+    [SerializeField]
+    private float distanceToAttack = 5f;
+    [SerializeField]
+    private float movementSpeed = 3f;
+    private Vector3 spawnPoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        characterController = GetComponent<CharacterController>();
+        rb = GetComponent<Rigidbody>();
+        spawnPoint = transform.position;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(PlayerClose())
+            AttackPlayer();
+    }
+
+    private bool PlayerClose()
+    {
+        if(Vector3.Distance(player.transform.position, transform.position)<=distanceToAttack)
+        {
+            return true;
+        }else
+        {
+            return false;
+        }
+    }
+    private void AttackPlayer()
+    {
+        Vector3 direction = (player.transform.position - transform.position).normalized;
+        rb.velocity = direction*movementSpeed*Time.deltaTime;
+    }
+    public void Respawn()
+    {
+        transform.position = spawnPoint;
+        rb.velocity = Vector3.zero;
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Transform spawnPoint;
+    private Rigidbody rb;
+    protected Joystick joystick;
+    private Vector3 movementDirection;
+    private Vector3 respawnPosition;
+    [SerializeField]
+    private float movementSpeed = 5f;
+    private float horizontalAxis,verticalAxis;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        joystick = FindObjectOfType<Joystick>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveWithJoystick();
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        Debug.Log("collider " + collider.transform.name);
+        if(collider.transform.tag=="Enemy")
+        {
+            Respawn();
+            collider.transform.GetComponent<Enemy>().Respawn();
+        }
+    }
+    private void MoveWithJoystick()
+    {
+        horizontalAxis = joystick.Horizontal;
+        verticalAxis = joystick.Vertical;
+        movementDirection = new Vector3(horizontalAxis, 0.0f, verticalAxis);
+        movementDirection *= movementSpeed;
+        rb.velocity = movementDirection*Time.fixedDeltaTime;
+    }
+
+    private void Respawn()
+    {
+        transform.position = spawnPoint.position;
+    }
+}
